21031c3a275c9a34b8d02a5e9c2f55e10641ab34
nahataichanok rojcharoenkul <alone_l3@hotmail.com>
2015-10-30 16:31:32 +0700

    score view

---
+++ TwoZeroFourEightScoreView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace twozerofoureight
+{
+    public partial class TwoZeroFourEightScoreView : Form
+    {
+        public TwoZeroFourEightScoreView()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
